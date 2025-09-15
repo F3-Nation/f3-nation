15f531c68c0564fc65e67a44f2c452943e6eb956
Declan Nishiyama <declan@mountaindev.com>
2025-09-15 07:48:34 -0400

    fix endpoint

--- packages/api/src/router/api.ts
+++ packages/api/src/router/api.ts
@@ -1,15 +1,10 @@
 import { revalidatePath } from "next/cache";
 
-import { apiKeyProcedure, publicProcedure } from "../shared";
+import { apiKeyProcedure } from "../shared";
 
 export const apiRouter = {
   revalidate: apiKeyProcedure.handler(async () => {
     revalidatePath("/");
     return Promise.resolve();
   }),
-  docs: publicProcedure
-    .route({ method: "GET", path: "/" })
-    .handler(async () => {
-      return Promise.resolve();
-    }),
 };
