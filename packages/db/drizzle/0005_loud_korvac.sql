5056bb4b024009868dc9e68bb05e0e138dc568ba
Declan Nishiyama <declan@mountaindev.com>
2025-11-30 18:08:20 -0500

    Update 0005_loud_korvac.sql
    Remove duplicate ao_count

--- packages/db/drizzle/0005_loud_korvac.sql
+++ packages/db/drizzle/0005_loud_korvac.sql
@@ -9,5 +9,5 @@ ALTER TABLE "achievements" ADD COLUMN "meta" json;--> statement-breakpoint
 ALTER TABLE "event_instances" ADD COLUMN "is_private" boolean DEFAULT false NOT NULL;--> statement-breakpoint
 ALTER TABLE "event_tags" ADD COLUMN "is_active" boolean DEFAULT true NOT NULL;--> statement-breakpoint
 ALTER TABLE "events" ADD COLUMN "is_private" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE "orgs" ADD COLUMN "ao_count" integer DEFAULT 0;--> statement-breakpoint
+-- ALTER TABLE "orgs" ADD COLUMN "ao_count" integer DEFAULT 0; -- removing broken command. It is already applied in the db (0004) but not in the schema.json
 ALTER TABLE "positions" ADD COLUMN "is_active" boolean DEFAULT true NOT NULL;
