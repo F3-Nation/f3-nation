d7094baf5fead915ee578f807e9f6309080b8bb2
Evan Petzoldt <evan.petzoldt@protonmail.com>
2026-01-07 12:47:08 +0000

    changing locations of api docs route to work locally

--- apps/api/src/app/api/docs/route.ts
+++ apps/api/src/app/api/docs/route.ts
@@ -5,7 +5,7 @@ import { env } from "@acme/env";
 export const GET = async () => {
   const baseUrl = env.NEXT_PUBLIC_API_URL;
   const response = ApiReference({
-    url: "/docs/openapi.json",
+    url: "/api/docs/openapi.json",
     baseServerURL: baseUrl,
     pageTitle: "F3 Nation API Reference",
   })();
