b256cb394c4444345be0869e6d42b7ff5b11394d
Tackle <damon.vinciguerra@gmail.com>
2026-05-12 15:26:26 -0500

    fix(me): allow for existing avatar URLs

--- apps/me/next.config.ts
+++ apps/me/next.config.ts
@@ -7,12 +7,25 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "storage.googleapis.com",
+        pathname: "/backblast-images/**",
+      },
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
         pathname: "/f3-public-images/**",
       },
       {
         protocol: "https",
         hostname: "storage.googleapis.com",
         pathname: "/f3-public-images-staging/**",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.slack-edge.com",
+      },
+      {
+        protocol: "https",
+        hostname: "a.slack-edge.com",
       },
     ],
   },
