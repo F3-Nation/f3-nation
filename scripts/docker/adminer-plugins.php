174f70cc0a8e66a6c6da746932e7939adabe0175
tackle <Damon.Vinciguerra@gmail.com>
2026-05-22 11:42:13 -0500

    fix(repo): nested adminer permanentlogin function

--- scripts/docker/adminer-plugins.php
+++ scripts/docker/adminer-plugins.php
@@ -1,13 +1,13 @@
 <?php
-class AdminerPermanentLogin extends Adminer {
-    function permanentLogin($create = false) {
-        return "f3-local-dev-key";
-    }
-    function name() {
-        return "F3 Nation DB";
-    }
-}
 function adminer_object() {
+    class AdminerPermanentLogin extends Adminer {
+        function permanentLogin($create = false) {
+            return "f3-local-dev-key";
+        }
+        function name() {
+            return "F3 Nation DB";
+        }
+    }
     return new AdminerPermanentLogin;
 }
 include './adminer.php';
