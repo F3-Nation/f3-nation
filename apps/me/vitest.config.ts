ce197deab904d85c2579a848769bf1c959c64f0c
tackle <Damon.Vinciguerra@gmail.com>
2026-07-09 08:40:34 -0500

    test(me): fixing local/ci discrepancy

--- apps/me/vitest.config.ts
+++ apps/me/vitest.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
-    env: { NODE_ENV: "test" },
+    env: { NODE_ENV: "test", SKIP_ENV_VALIDATION: "1" },
     include: ["src/**/*.test.{ts,tsx}", "__tests__/**/*.test.{ts,tsx}"],
     coverage: {
       provider: "v8",
@@ -17,10 +17,10 @@ export default defineConfig({
       exclude: coverageExclude,
       thresholds: {
         autoUpdate: true,
-        statements: 40.77,
-        branches: 40.03,
-        functions: 21.87,
-        lines: 42.2,
+        statements: 42.13,
+        branches: 40.35,
+        functions: 26.04,
+        lines: 43.74,
       },
     },
     setupFiles: ["./vitest.setup.ts"],
